cb411040523f614796db02e05b649b3deca00d94
brianorora <brianorora@gmail.com>
2022-01-12 22:17:36 +0300

    Add test for posts#show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Posts')
     end
   end
+
+  describe 'GET #index' do
+    before(:each) { get user_post_path user_id: 32, id: 54 }
+    it 'should return correct response' do
+      expect(response).to have_http_status(200)
+    end
+    it 'should render the correct template' do
+      expect(response).to render_template(:show)
+    end
+    it 'should have the text Posts' do
+      expect(response.body).to include('Single Post')
+    end
+  end
+
 end
