4875cb70169bee79a3af25a2e6f1d532968c7fb7
brianorora <brianorora@gmail.com>
2022-01-13 12:01:07 +0300

    Add posts migration

---
+++ db/migrate/20220113085205_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.text :title
+      t.string :text
+      t.integer :comments_counter
+      t.integer :likes_counter
+      t.timestamps
+    end
+  end
+end
