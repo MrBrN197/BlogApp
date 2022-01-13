b110f9eede2423a04ad7bc6e900d40a46d0d8a7c
brianorora <brianorora@gmail.com>
2022-01-13 18:40:27 +0300

    Add foreign keys to Comments migration
    - author_id key
    - post_id key

--- db/migrate/20220113090246_create_comments.rb
+++ db/migrate/20220113090246_create_comments.rb
@@ -2,6 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :text
+      t.references :author
+      t.references :post
 
       t.timestamps
     end
