2ca08486c390149f2298ff2b0ad8d6167eee5a1e
brianorora <brianorora@gmail.com>
2022-01-13 19:12:27 +0300

    Set foreign keys

--- db/migrate/20220113090246_create_comments.rb
+++ db/migrate/20220113090246_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :text
-      t.references :author
-      t.references :post
+      t.references :author, foreign_key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220113090510_create_likes.rb
+++ db/migrate/20220113090510_create_likes.rb
@@ -2,7 +2,7 @@ class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
       t.references :author, foreign_key: { to_table: 'users'}
-      t.references :post
+      t.references :post, foreign_key: true
     end
   end
 end
