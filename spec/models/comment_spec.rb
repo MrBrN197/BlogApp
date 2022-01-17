be46de4ddbcf63d65d9d900acb40aa1c0915ebeb
brianorora <brianorora@gmail.com>
2022-01-17 13:07:28 +0300

    Add test for comment model

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  it 'add some examples to (or delete)' do
+    author = User.new(name: 'Mandy', photo: '', bio: '', posts_counter: 0)
+    post = Post.new(title: 'Frustrating...', text: 'I haven\'t completed things...', author: author)
+    post.save!
+
+    comment_creator = User.new(name: 'Mike', photo: '', bio: '', posts_counter: 0)
+    post.comments.create!(text: 'nice one', author: comment_creator)
+    post.comments.create!(text: 'really nice post', author: comment_creator)
+
+    Comment.update_comments_counter_for(post)
+    expect(post.comments.length).to eql(2)
+  end
+end
