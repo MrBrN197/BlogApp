88fd95cf119a2c0dcf7cc63c0460955aced15a7d
brianorora <brianorora@gmail.com>
2022-01-18 16:41:51 +0300

    Add view for post#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,11 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @posts = @user.first_three
   end
 
-  def show; end
+  def show
+    user = User.find(params[:user_id])
+    @post = user.posts.find(params[:id])
+  end
 end
