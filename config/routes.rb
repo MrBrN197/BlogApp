913d48052d9f639a5ea323feccc343388a624886
brianorora <brianorora@gmail.com>
2022-01-20 16:54:05 +0300

    Add route for create

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new]
+    resources :posts, only: [:index, :show, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
