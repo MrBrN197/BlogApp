ad85a12b5754a2100bbf59ba895d0026c7497a01
brianorora <brianorora@gmail.com>
2022-01-20 10:07:29 +0300

    Modify posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,8 @@ class PostsController < ApplicationController
   end
 
   def show
-    # user = User.find(params[:user_id])
-    # @post = user.posts.find(params[:id])
-    @post = Post.find(params[:id])
+    user = User.find(params[:user_id])
+    @post = user.posts.find(params[:id])
   end
 
   def new
