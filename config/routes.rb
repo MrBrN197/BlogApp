0990a72b4277e633cb07b28d5be06ca604715608
brianorora <brianorora@gmail.com>
2022-01-21 01:50:50 +0300

    Add like button

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       post 'add_comment', to: 'comments#create', on: :member
+      post 'add_like', to: 'likes#create', on: :member
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
