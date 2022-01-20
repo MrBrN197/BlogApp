11b42f26a365e644186ac9639350e258b89f898a
brianorora <brianorora@gmail.com>
2022-01-21 01:47:29 +0300

    Add functionality for creating a comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,9 @@ class PostsController < ApplicationController
   end
 
   def show
-    user = User.find(params[:user_id])
-    @post = user.posts.find(params[:id])
+    # user = User.find(params[:user_id])
+    # @post = user.posts.find(params[:id])
+    @post = Post.find(params[:id])
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      post 'add_comment', to: 'comments#create', on: :member
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
