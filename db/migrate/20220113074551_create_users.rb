da005fefa52922f6a2d7d7dac242481af18d3f1e
brianorora <brianorora@gmail.com>
2022-01-21 21:49:34 +0300

    Update database schema.rb

--- db/migrate/20220113074551_create_users.rb
+++ db/migrate/20220113074551_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.text :name
+      t.text :name, null: false
       t.string :photo
       t.string :bio
-      t.integer :posts_counter
+      t.integer :posts_counter, default: 0
       t.timestamps
     end
   end
--- db/migrate/20220113085205_create_posts.rb
+++ db/migrate/20220113085205_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration[6.1]
     create_table :posts do |t|
       t.text :title, null: false
       t.string :text, null: false
-      t.integer :comments_counter
-      t.integer :likes_counter
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
 
       t.references :author, foreign_key: { to_table: 'users'}, null: false
 
