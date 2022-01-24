ddd08aae202cf1e922d923c66fbc1c925d0bd93f
leolpaz <leonardolpaz95@gmail.com>
2022-01-24 11:52:00 -0300

    Add confirmation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         :confirmable
 
   # Validation
   validates :name, presence: true
