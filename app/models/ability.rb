5ec26de71a813be65e78f5c9c6ae74cd736886c5
brianorora <brianorora@gmail.com>
2022-01-26 02:19:31 +0300

    Modify user abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,8 @@ class Ability
     can :read, Comment
     return unless user.present?
 
-    can :manage, Post, author_id: user.id
-    can :manage, Comment, author_id: user.id
-
+    can :destroy, Post, author_id: user.id
+    can :destroy, Comment, author_id: user.id
     return unless user.admin?
 
     can :manage, :all
